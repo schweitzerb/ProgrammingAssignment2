4f80071528328a9f0686f03cfb472fe8f2ac20b1
Ben S <schweitzerb@gmail.com>
2014-07-23 12:35:48 -0400

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,22 @@
 ## input matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-      inverse <- NULL
       
-      set <- function(y){
+      inverse <- NULL ##assign NULL as inverse has not yet been calcualted
+      
+      ##function to enable updating the input matrix
+      set <- function(y){ 
             x <<- y
-            inverse <<- NULL
+            inverse <<- NULL ##reset the inverse since the matrix has changed
       }
       
+      ##function to return the matrix 
       get <- function() x
+      
+      ##function to set the inverse
       setinverse <- function(updatedInverse) inverse <<- updatedInverse
+      
+      ##function to return the inverse of the input matrix
       getinverse <- function() inverse
       
       list(set = set, get = get, 
@@ -21,18 +28,23 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## returns the inverse of the input matrix, either from chache or by using solve()
+## returns the inverse of the input matrix, either from chache or by using 
+## solve() to calcualate the inverse
 
 cacheSolve <- function(x, ...) {
-        checkInverse <- x$getinverse()
-        
-        if(!is.null(checkInverse)){
-              message("Getting cached data")
-              return(m)
-        }
-        
-        matrix <- x$get()
-        newInverse <- solve(matrix)
-        x$setinverse(newInverse)
-        
+      
+      ##check to see if the inverse is already cached, return it if true
+      checkInverse <- x$getinverse()          
+      if(!is.null(checkInverse)){ 
+            message("Getting cached data...")
+            return(m)
+      }
+      
+      ##calculate the inverse (will only execute if it wasn't found in cache)
+      matrix <- x$get()
+      newInverse <- solve(matrix)
+      x$setinverse(newInverse)
+      newInverse
 }
+
+
